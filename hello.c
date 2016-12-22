abb19dcfa55e8581a9d852c7c00fec8da5f01c39
Abigail Marks <marksa19@lsrhs.net>
2016-12-22 13:28:33 +0000

    changed

--- hello.c
+++ hello.c
@@ -1,9 +1,10 @@
-#include <cs50>
+#include <cs50.h>
 #include <stdio.h> 
 
 int main(void)
 {
     printf("Enter your name:");
     string name = GetString();
-    printf("Hello, %s\n", name);
+    printf("HEllo, %s\n", name);
+    
 }
